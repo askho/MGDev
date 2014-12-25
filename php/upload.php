0a99db15e87469b78c2c3a419c8c383539ddd518
SeanAvena <sean.avena@gmail.com>
2014-12-24 16:56:13 -0800

    completed basic file upload
    posts json for the location of each image, and posts the album name.
    files cannot be removed on this page.
    save location for uploads in now in images/photos.
    relocated upload.php to php folder

---
+++ php/upload.php
@@ -0,0 +1,67 @@
+<?php
+
+$createdNewDir = 0;
+$ds          = DIRECTORY_SEPARATOR;  //1
+
+$storeFolder = ".." .$ds . 'images' . $ds . "photos";   //2
+
+if (!empty($_FILES)) {
+
+    $tempFile = $_FILES['file']['tmp_name'];          //3             
+
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+
+    $randName = chr( mt_rand( 97 ,122 ) ) .substr( md5( time( ) ) ,1 ) . "." . pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+    
+    $targetFile =  $targetPath. $randName;  //5
+    
+    move_uploaded_file($tempFile,$targetFile); //6
+    
+    echo 'images' . $ds . "photos" . $ds .$randName;
+    //echo $storeFolder . $ds . $randName;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// not needed atm
+
+function createTempDir(){
+    // get currrent directory
+    $curDir = getcwd();
+    echo "current directory: " . $curDir ;
+    echo "<br>";
+
+    // generate temp folder
+
+    // create new directory name
+    $randomString = chr( mt_rand( 97 ,122 ) ) .substr( md5( time( ) ) ,1 );
+    echo "name for new directory: " . "\\uploads\\" . $randomString;
+    echo "<br>";
+
+    // create new directory
+    $newDir = $curDir . "\\uploads\\" . $randomString;
+    while(!$createdNewDir){
+        if (!file_exists($newDir)) {
+            mkdir($newDir, 0777, true);
+            $createdNewDir = 1;
+        }
+    }
+    echo "created new directory";
+    echo "<br>";
+
+    // set location to store photos
+    $storeFolder = "\\uploads\\" . $randomString . "\\";   //2
+    echo $storeFolder;
+    echo "<br>";
+}
+
+?> 
