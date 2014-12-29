fadbbdf3e75f0c203ad187e92aa51f55386c3adc
SeanAvena <sean.avena@gmail.com>
2014-12-28 23:17:58 -0800

    added the re-enter password field
    also completed its functionality

--- admin/change_password.php
+++ admin/change_password.php
@@ -7,12 +7,17 @@ require './php/logged_in.php';
         <form action="php/change_password.php" method="post" enctype= "multipart/form-data">
             <div class = "form-group">
                 <label>Current Password<br>
-                    <input type="password" name="current_pass" id = "category">
+                    <input type="password" name="current_pass" id = "oldPass">
                 </label>
             </div>
             <div class = "form-group">
                 <label>New Password<br>
-                    <input type="password" name="new_pass" id = "albumName">
+                    <input type="password" name="new_pass" id = "newPass">
+                </label>
+            </div>          
+            <div class = "form-group">
+                <label>Re-enter Password<br>
+                    <input type="password" name="confirm_pass" id = "confirmPass">
                 </label>
             </div>
             <button type="submit" id = "submit">Change Password</button>
--- admin/php/change_password.php
+++ admin/php/change_password.php
@@ -13,49 +13,78 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// variables
-$currPass = $_POST["current_pass"];
-$newPass = $_POST["new_pass"];
+// change the password or alert error
+echo changePass();
 
-// check that fields aren't empty
-if(empty($currPass) || empty($newPass)){
-    echo "<script> 
+
+// ---------- FUNCTIONS ----------
+
+/* Validates the password values entered into the form and changes 
+the password or returns a string of javascript to alert the error
+and redirect back to the change password page. 
+*/
+function changePass(){
+    // variables
+    $currPass = $_POST["current_pass"];
+    $newPass = $_POST["new_pass"];
+    $confirmPass = $_POST["confirm_pass"];
+
+    // check that fields aren't empty
+    if(empty($currPass) || empty($newPass) || empty($confirmPass)){
+        return "<script> 
         window.alert('please enter passwords and try again');
         window.location.replace(\"../change_password.php\");
         </script>";
-}
+    }
 
-// Query database - sql injection protected
-$sql = "SELECT username, hash 
+    // check that new password and re-entry match
+    if (strcmp($newPass, $confirmPass) && !empty($newPass) && !empty($confirmPass)){
+        return "<script> 
+        window.alert('Your new password and re-entry do not match');
+        window.location.replace(\"../change_password.php\");
+        </script>";
+    }
+
+    // Query database - sql injection protected
+    $sql = "SELECT username, hash 
         FROM Admin 
         WHERE username = 'mike'
         OR username = 'leon'
         OR username = 'sean'";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$stmt->bind_result($fUser, $fHash);
+    global $conn;
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $stmt->bind_result($fUser, $fHash);
 
-// compare with known admin passwords
-while($stmt->fetch()){
-    // check that current password was entered
-    if ( hash_equals($fHash, crypt($currPass, $fHash)) ) {
-        // generate new hashed password
-        $hashPass = randHashPass($newPass);
-        $stmt->close();
+    // compare with known admin passwords
+    while($stmt->fetch()){
+        // check that current password was entered
+        if ( hash_equals($fHash, crypt($currPass, $fHash)) ) {
+            // generate new hashed password
+            $hashPass = randHashPass($newPass);
+            $stmt->close();
 
-        // update to new password - sql injection protected
-        $sql = "UPDATE Admin
+            // update to new password - sql injection protected
+            $sql = "UPDATE Admin
         SET hash = ? 
         WHERE username = 'mike'";
-        $stmt2 = $conn->prepare($sql);
-        $stmt2->bind_param("s", $hashPass);
-        $stmt2->execute();
-        $stmt2->close();
-        
-        // diplsay success and return to control panel
-        echo "<script> 
+            $stmt2 = $conn->prepare($sql);
+            $stmt2->bind_param("s", $hashPass);
+            $stmt2->execute();
+            $stmt2->close();
+
+            // diplsay success and return to control panel
+            return "<script> 
         window.alert('password changed successfully');
         window.location.replace(\"../control_panel.php\");
+        </script>";
+        }
+    }
+
+    if(!$stmt->fetch()){
+        return "<script> 
+        window.alert('Current password incorrectly entered');
+        window.location.replace(\"../change_password.php\");
         </script>";
     }
 }
