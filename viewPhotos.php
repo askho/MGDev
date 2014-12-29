ad6a572449c8719592ed00d7565786eb368e6d6f
Leon Ho <leon.lai.ho@gmail.com>
2014-12-29 11:55:10 -0800

    Light box now working
    Has some issues with viewing on mobile.(Does scale very well)

--- viewPhotos.php
+++ viewPhotos.php
@@ -4,7 +4,7 @@
   	<title>Home</title>
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<script src="js/isotope.js"></script>
 	<script src="js/main.js"></script>
