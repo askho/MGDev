9e936f52ef305a0a260922437bcdf4eaa3ff3d92
Leon Ho <leon.lai.ho@gmail.com>
2015-01-10 17:22:37 -0800

    Added view album
    POST in a category name with key "categoryName" and it
    will echo out the albumID, and album name of all albums
    inside that category! If no category is inputed it will output all
    albums.

---
+++ admin/php/viewAlbum.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+		POST in a category name with key "categoryName" and it
+		will echo out the albumID, and album name of all albums 
+		inside that category! If no category is inputed it will output all
+		albums.
+	*/
+	require '../../php/connection.php';
+	if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+	}
+	$output = array();
+	$sql = "SELECT albumID, albumName FROM album NATURAL JOIN albumCategory NATURAL JOIN category";
+	if(isset($_POST['categoryName'])) {
+		$categoryName = $_POST['categoryName'];
+		$sql = $sql." WHERE categoryName = '$categoryName'";
+	}
+	$result = mysqli_query($conn, $sql);
+	while($rows = mysqli_fetch_assoc($result)) {
+		$output[] = $rows;
+	}
+	echo json_encode($output);
+	mysqli_close($conn);
+?>
