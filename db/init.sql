99d69d61c4cb5881a4ec12cb3add02e3286643bd
SeanAvena <sean.avena@gmail.com>
2015-01-11 17:43:07 -0800

    editphotos page with ajax, updated initdb

--- db/init.sql
+++ db/init.sql
@@ -16,7 +16,7 @@ CREATE TABLE Photo (
 	,ISO VARCHAR(15) NULL
 	,focalLength INT NULL
 	,camera VARCHAR(50) NULL
-	,description BLOB NULL
+	,description TEXT NULL
 	,location VARCHAR(100) NULL
 	,originalFileName VARCHAR(100) NULL
 	,CONSTRAINT pk_photoID PRIMARY KEY (photoID)
